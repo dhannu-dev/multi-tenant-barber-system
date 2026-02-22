9ace5cf15da9194dd03f8d510b8b2ae44a10ccf1
dhannu-dev <krdhanak47@gmail.com>
2026-02-22 11:21:51 +0530

    origin change

--- barber-backend/index.js
+++ barber-backend/index.js
@@ -2,10 +2,10 @@ import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
 
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
-// const hash = await bcrypt.hash("admin123", 10);
-// console.log(hash);
+const hash = await bcrypt.hash("admin123", 10);
+console.log(hash);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- barber-backend/src/app.js
+++ barber-backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: true,
+    origin: "https://multi-tenant-barber-system.vercel.app",
     credentials: true,
   }),
 );
