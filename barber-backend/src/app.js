66f9ba30568a3f800cfca6d8e81456be8e536997
dhannu-dev <krdhanak47@gmail.com>
2026-02-23 10:18:15 +0530

    adding route health route

--- barber-backend/src/app.js
+++ barber-backend/src/app.js
@@ -19,6 +19,10 @@ app.use(
   }),
 );
 
+app.get("/", (req, res) => {
+  res.status(200).send("Backend is running 🚀");
+});
+
 app.use("/api/tenants", tenantRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/services", serviceRoutes);
